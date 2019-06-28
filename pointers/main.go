1c6194a5d5da8fe62449a77e10e64573f99cd5c4
Arturo Fernandez <artufm at yahoo dot es>
2019-06-28 16:00:48 -0700

    Adding more example for pointers/structs

--- pointers/main.go
+++ pointers/main.go
@@ -10,10 +10,14 @@ type Persona struct {
 
 // changeName will change `first` using a pointer receiver
 // There are two reasons to use a pointer receiver:
-// 	1) The method can modify the value that its receiver points to
-//  2) To avoid copying the value on each method call. This can be more efficient
-//     if the receiver is a large struct, for example.
+//  1) The method can modify the value that its receiver points to
+//  2) To avoid copying the value on each method call. This can be more efficient if the receiver is a large struct
 func (p *Persona) changeName(newName string) {
+	p.first = newName
+}
+
+// changeName2 won't change original struct
+func (p Persona) changeName2(newName string) {
 	p.first = newName
 }
 
@@ -41,9 +45,22 @@ func main() {
 	// Returning a pointer to the struct value.
 	persona := &Persona{"Foo", "Bar"}
 	fmt.Println(persona.first)
+	fmt.Println((*persona).first)
+
+	// You can use `&` with same effect that lines above. Go figure that out for you
+	persona2 := Persona{"Foo2", "Bar2"}
+	fmt.Println(persona2.first)
+	// Next line won't compile because we used `Persona` and not `&Persona`
+	// fmt.Println((*persona2).first)
 
 	// Structs and pointers
 	persona.changeName("New name")
+	fmt.Println(persona.first)
+
+	persona2.changeName("New name 2")
+	fmt.Println(persona2.first)
+
+	persona.changeName2("The new name")
 	fmt.Println(persona.first)
 }
 
