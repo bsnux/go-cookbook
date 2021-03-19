cd4b6ef4b0c3fccbd5b1d63a130a4ee154f84bef
bsnux <>
2021-03-18 21:32:59 -0700

    simple ping/pong HTTP server

---
+++ pinghtml/go.mod
@@ -0,0 +1,3 @@
+module pinghtml
+
+go 1.14
---
+++ pinghtml/main.go
@@ -0,0 +1,21 @@
+// Simple web server listening on port 8080
+//
+// Launch this program, open a new terminal, then type:
+// `curl -L http://localhost:8080`
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><body>pong</body></html>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("Starting server...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
