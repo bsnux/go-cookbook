be770a1298c82ee63a718478a6d5b085dfb75755
Arturo Fernandez <134515+bsnux@users.noreply.github.com>
2023-05-10 12:13:36 -0700

    adding Go pointer receiver examples

--- pointerecv/main.go
+++ pointerecv/main.go
@@ -35,26 +35,31 @@ func (user User) displayAge() {
 }
 
 func main() {
-	fmt.Println("Hello, playground")
-
 	user := User{23, "Lucas"}
-
 	fmt.Println(user)
-
 	user.age = 25
 	fmt.Println(user)
 	fmt.Println(user.age)
 
 	user.changeAge(30)
 	fmt.Println(user)
-
+	// Following line is equivalent to `user.changeAge(50)`, Go does the work for us
+	(&user).changeAge(50)
+	fmt.Println(user)
 	user.displayAge()
 
-	school := School{"USC"}
+	bill := &User{40, "Bill"}
+	fmt.Println(bill.name)
 
+	bill.changeAge(44)
+	fmt.Println(bill.age)
+	// Following line is equivalent to `bill.changeAge(55)`, Go does the work for us
+	(*bill).changeAge(55)
+	fmt.Println(bill.age)
+
+	// Composition example,
+	school := School{"USC"}
 	student := Student{school, user}
 	fmt.Println(student)
-
 	fmt.Println(student.user.name)
-
 }
